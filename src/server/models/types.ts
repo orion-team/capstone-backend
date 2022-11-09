b6225b5f46e745d190be4064298fe2cee36eaa3e
Ksenia Kold <kseniacold@gmail.com>
2022-11-08 17:57:13 -0800

    feat: adjust types for v2

--- src/server/models/types.ts
+++ src/server/models/types.ts
@@ -1,35 +1,39 @@
+const imageSize = {
+  thumbnail: "THUMBNAIL",
+  small: "SMALL",
+  regular: "REGULAR",
+  large: "LARGE",
+} as const;
+
+export type ImageKey = typeof imageSize[keyof typeof imageSize];
+export interface Image {
+  url: string;
+  width: number;
+  height: number;
+}
+
 export interface Recipe {
   uri: string;
   label: string;
   image: string;
   // Source site identifier
   source: string;
-
   // Original recipe URL
   url: string;
-}
-
-export interface Food {
-  foodId: string;
-  label: string;
-}
-
-export interface Measure {
-  // Ontology indetifier
-  uri: string;
-
-  // Common Name
-  label: string;
+  images: Record<ImageKey, Image>;
+  ingredientLines: string[];
 }
 
 export interface Ingedient {
-  foodId: string;
-  quantity: number;
-  measure: Measure;
-  weight: number;
-  food: Food;
+  text?: string;
+  quantity?: number;
+  measure?: string;
+  food?: string;
+  weight?: number;
   // shopping isle category
-  foodCategory: string;
+  foodCategory?: string;
+  foodId?: string;
+  image?: string;
 }
 
 export interface User {
