680ce3910ce9236a321d49ba23c5a1cce9d20abf
Ksenia Kold <kseniacold@gmail.com>
2022-11-25 16:12:24 -0800

    feat: implement post login

--- src/server/routes/auth/postLogin.ts
+++ src/server/routes/auth/postLogin.ts
@@ -1,8 +1,23 @@
+import { randomUUID } from "crypto";
+import mysql from "mysql";
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { OAuth2Client, TokenPayload } from "google-auth-library";
+import db from "../../DBConnection";
 
 const client = new OAuth2Client(process.env.CLIENT_ID);
+
+const queryUserInsert = (email: string, name: string, picture: string) => `
+INSERT INTO user (id, email, name, picture) VALUES (UUID_TO_BIN(UUID()), ${mysql.escape(
+  email
+)}, ${mysql.escape(name)}, ${mysql.escape(picture)}); 
+`;
+
+const queryUserGet = (email: string) => `
+SELECT BIN_TO_UUID(id) as uuid, email, name, picture FROM user WHERE email=${mysql.escape(
+  email
+)}
+`;
 
 export const postLogin = async (req: Request, res: Response) => {
   const errors = validationResult(req);
@@ -11,20 +26,40 @@ export const postLogin = async (req: Request, res: Response) => {
   }
 
   const { token } = req.body;
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
 
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: process.env.GOOGLE_CLIENT_ID,
-  });
-  const { name, email, picture }: TokenPayload = ticket.getPayload();
+    const {
+      name = "",
+      email = "",
+      picture = "",
+    }: TokenPayload = ticket.getPayload();
 
-  // TODO replace with user id from database
-  req.session["email"] = email;
-  req.session["name"] = name;
+    if (!email) {
+      res.status(404).send("Can not find user email");
+    }
 
-  res.status(200).json({
-    name,
-    email,
-    picture,
-  });
+    let resultsExistingUser = await db.query(queryUserGet(email));
+
+    if (
+      !Array.isArray(resultsExistingUser) ||
+      resultsExistingUser.length === 0
+    ) {
+      await db.query(queryUserInsert(email, name, picture));
+      resultsExistingUser = await db.query(queryUserGet(email));
+    }
+
+    req.session.userId = resultsExistingUser[0].uuid;
+    res.status(200).json({
+      email,
+      name,
+      picture,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
 };
